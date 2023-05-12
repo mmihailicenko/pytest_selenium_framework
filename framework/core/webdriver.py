25c652a2a11c750eb66532290e7e975b1077c702
Mihails Mihailicenko <Mihails.Mihailicenko@EntainGroup.com>
2023-05-12 13:44:35 +0200

    gitlab yaml is added

--- framework/core/webdriver.py
+++ framework/core/webdriver.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-class WebDriver(webdriver.Chrome):
+class WebDriver(webdriver.Remote):
     def __init__(self):
-        super().__init__()
+        super().__init__(
+            command_executor='http://selenium-hub:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME
+        )
         self.implicitly_wait(5)
         self.set_page_load_timeout(30)
         self.maximize_window()
